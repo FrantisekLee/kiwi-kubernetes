765708c3dbdf5653879e230f857c80d1117878c6
Frantisek Lee <frantisek.lee@gmail.com>
2019-03-21 22:03:36 +0100

    kiwi-projects

---
+++ terraform/kiwi-gcloud-cluster-sql_DB.tf
@@ -0,0 +1,12 @@
+resource "google_sql_database_instance" "master" {
+  name = "master-instance"
+  database_version = "MYSQL_5_6"
+  # First-generation instance regions are not the conventional
+  # Google Compute Engine regions. See argument reference below.
+  region = "europe-west1"
+
+  settings {
+    tier = "D0"
+  }
+}
+
